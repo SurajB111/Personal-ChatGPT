835b16b958bdd101fe7a4ff57a23f242338a47e3
SurajB111 <surajbisht.as1234@gmail.com>
2023-05-24 12:24:27 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -81,7 +81,7 @@ const handleSubmit = async (e) => {
     const messageDiv = document.getElementById(uniqueId);
 
     loader(messageDiv);
-    const response = await fetch('http://localhost:5000',{
+    const response = await fetch('https://personal-chatgpt-5hdb.onrender.com',{
         method: 'POST',
         headers:{
             'Content-type': 'application/json'
